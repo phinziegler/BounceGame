030721975c0e872689377d2c4d38e2163c59f019
Phineas Ziegler <phinziegler@gmail.com>
2021-12-26 23:23:30 -0600

    added vector

--- src/gameObjects/gameObject.js
+++ src/gameObjects/gameObject.js
@@ -1,5 +1,12 @@
-export default class GameObject {
-    constructor() {
+import Vector from "../tools/vector";
 
+// Abstract Version of a Game Object
+export default class GameObject {
+    constructor(x, y) {
+        this.position = new Vector(x, y);
+    }
+
+    draw() {
+        throw new Error("draw() unimplemented");
     }
 }
---
+++ src/tools/vector.js
@@ -0,0 +1,22 @@
+export default class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // GET-SET X
+    get x() {
+        return this.x;
+    }
+    set x(x) {
+        this.x = x;
+    }
+
+    // GET-SET Y
+    get y() {
+        return this.y;
+    }
+    set y() {
+        this.y = y;
+    }
+}
