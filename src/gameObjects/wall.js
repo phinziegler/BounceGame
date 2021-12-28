dd5d65856a976fab7df970ef7994f8fa5948551e
Phineas Ziegler <phinziegler@gmail.com>
2021-12-28 15:57:08 -0600

    cleaned up wall class

--- src/gameObjects/wall.js
+++ src/gameObjects/wall.js
@@ -6,16 +6,19 @@ export default class Wall extends GameObject {
         super(canvas, x, y, mass);
         this.color = color;
         this.width = width;
+
+        // CONSTANTS
         this.friction = .998;
     }
 
+    // DRAW
     draw() {
         this.ctx.fillStyle = this.color;
         this.ctx.fillRect(this.position.x - (this.width / 2), 0, this.width, this.gameHeight);
     }
 
+    // FRAME UPDATE
     update(deltaTime, objects) {
-
         const oldVelX = this.velocity.x;
         const oldVelY = this.velocity.y;
         
@@ -26,44 +29,49 @@ export default class Wall extends GameObject {
         this.velocity.x = this.velocity.x * this.friction;
         
         // Update Position... dX = (v0 + v / 2) * t
-        this.prevPos.x = this.position.x;
-        this.prevPos.y = this.position.y;
         this.position.x = this.position.x + (((oldVelX + this.velocity.x) / 2) * deltaTime);
         this.position.y = this.position.y + (((oldVelY + this.velocity.y) / 2) * deltaTime);
 
         this.collide(objects);
     }
 
-    distanceFrom(x, y) {
-        let left = Math.abs(this.position.x - (this.width / 2) - x);
-        let right = Math.abs(this.position.x + (this.width / 2) - x);
-        return Math.min(left, right);
-    }
-
+    
+    // COLLISION
     collide(objects) {
         this.collideRight();
         this.collideLeft();
         this.collideObject(objects);
     }
-
     collideRight() {
         if(this.position.x >= this.gameWidth) {
             this.position.x = this.gameWidth;
             this.velocity.x *= -1;
         }
     }
-
     collideLeft() {
         if(this.position.x <= 0) {
             this.position.x = 0;
             this.velocity.x *= -1;
         }
     }
+    
+    ////////////////////////
+    /// OBJECT COLLISION /// 
+    ////////////////////////
 
+    // parameter to determine if a distance is colliding with the wall
     distanceParameter() {
         return this.width/2;
     }
 
+    // distance from an input point to self -- only uses horizontal distance
+    distanceFrom(x, y) {
+        let left = Math.abs(this.position.x - (this.width / 2) - x);
+        let right = Math.abs(this.position.x + (this.width / 2) - x);
+        return Math.min(left, right);
+    }
+    
+    // normal direction from the wall to an input point -- returns horizontal normals
     collisionNormal(x, y) {
         let normal = new Vector(x - this.position.x, 0);
         return normal.unitVector();
